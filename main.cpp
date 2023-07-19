c58519a9ecff6981bab60001280882c2541e1832
breki <breki@directivegames.com>
2023-07-19 16:30:29 +0000

    boom 2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,18 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window)
+{
+    // close window on escape key press
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -13,7 +25,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    auto* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    auto* window = glfwCreateWindow(800, 600, "TRIANGLE!!!", nullptr, nullptr);
     if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -28,5 +40,23 @@ int main() {
         return -1;
     }
 
+    // set viewport size
+    glViewport(0, 0, 800, 600);
+
+    // resize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+    // render loop
+    while(!glfwWindowShouldClose(window))
+    {
+        process_input(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
