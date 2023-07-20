5b4419a6cbfd74b4c295b0768bedb77cfedbbc1e
breki <breki@directivegames.com>
2023-07-20 14:29:22 +0000

    draw triangle and move

--- main.cpp
+++ main.cpp
@@ -11,12 +11,104 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+const float speed = 0.01f;
+bool red = false;
+int backgroundInputDelay = 0;
+
+float triangle[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+};
+
+float square[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+};
+
+unsigned int squareIndices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+};
+
 void process_input(GLFWwindow* window)
 {
+    if (backgroundInputDelay > 0)
+    {
+        backgroundInputDelay--;
+    }
     // close window on escape key press
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+        if (red)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        triangle[0] += speed;
+        triangle[3] += speed;
+        triangle[6] += speed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        triangle[0] -= speed;
+        triangle[3] -= speed;
+        triangle[6] -= speed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        triangle[1] += speed;
+        triangle[4] += speed;
+        triangle[7] += speed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        triangle[1] -= speed;
+        triangle[4] -= speed;
+        triangle[7] -= speed;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && backgroundInputDelay == 0)
+    {
+        red = !red;
+        if (red)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+        backgroundInputDelay = 10;
+    }
 }
+
+const char* vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\n\0";
 
 int main() {
     glfwInit();
@@ -46,12 +138,62 @@ int main() {
     // resize callback
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    auto shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
 
     // render loop
     while(!glfwWindowShouldClose(window))
     {
         process_input(window);
+
+        // render commands
+        // state-setting function
+        if (red)
+        {
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+        else
+        {
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndices), squareIndices, GL_STATIC_DRAW);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
--- notes.md
+++ notes.md
@@ -1,1 +1,25 @@
 opengl is a state-machine
+
+VAO: Vertex Array Object
+
+VBO: Vertex Buffer Object
+
+EBO: Element Buffer Object
+
+```c++
+// shader debugging
+int  success;
+char infoLog[512];
+glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+if(!success)
+{
+glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+}
+//shader program debugging
+glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+if(!success) {
+glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+std::cout << "ERROR::SHADER_PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+}
+```
